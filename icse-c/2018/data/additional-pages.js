169c676008e901245c2f1aeaddd32bde6ef4324d
Ricardo Quintero <iscrquinter@gmail.com>
2018-06-04 14:20:22 -0600

    ICSE-C 2018

---
+++ icse-c/2018/data/additional-pages.js
@@ -0,0 +1,10 @@
+var additionalPages = [
+    {
+      pageName: "Sponsors",
+      fileLocation: "content/sponsors.html"
+    }/* ,
+    {
+      pageName: "A second Example",
+      fileLocation: "content/extra-content-example.html"
+    } */
+  ];
